cad0213de091911d36378013d1bb7859f3062338
Morgan Brown <brown.mhg+git@gmail.com>
2018-07-13 14:55:01 +0200

    Use new vue cli to create new project
    * Build builds to home.php

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+// vue.config.js
+module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].filename = '../site/templates/home.php'
+        return args
+      })
+
+    config
+      .output
+        .publicPath('/assets/')
+  }
+}
