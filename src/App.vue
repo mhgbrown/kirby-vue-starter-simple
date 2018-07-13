4788c49c2f9cc7007024383c019ccb7c223bf74e
Morgan Brown <brown.mhg+git@gmail.com>
2018-07-13 15:09:06 +0200

    Copy over contents of client to src, delete client
    * Add some fun default styles

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,179 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view :key="$route.path"></router-view>
   </div>
 </template>
 
+<script>
+import EventBus from './util/EventBus'
+import * as types from './store/mutation-types'
+
+export default {
+  name: 'app',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  beforeCreate () {
+    this.$store.commit(types.RECEIVE_PAGES, { pages: window.Kirby.pages })
+    this.$store.commit(types.RECEIVE_SITE, { site: window.Kirby.site })
+  },
+  created () {
+    window.addEventListener('resize', this.onResize, true)
+    window.addEventListener('scroll', this.onScroll)
+    window.addEventListener('orientationchange', this.onOrientationChange)
+
+    this.onResize()
+  },
+  destroyed () {
+    window.removeEventListener('resize', this.onResize, true)
+    window.removeEventListener('scroll', this.onScroll)
+    window.removeEventListener('orientationchange', this.onOrientationChange)
+  },
+  methods: {
+    onResize (event) {
+      var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+      this.$store.commit(types.UPDATE_APP, { field: 'tablet', value: windowWidth <= 960 })
+      this.$store.commit(types.UPDATE_APP, { field: 'phone', value: windowWidth <= 480 })
+
+      EventBus.$emit('app.resize', event)
+    },
+    onScroll (event) {
+      EventBus.$emit('app.scroll', event)
+    },
+    onOrientationChange (event) {
+      EventBus.$emit('app.orientationchange', event)
+    }
+  }
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+@import 'node_modules/reset-css/sass/_reset';
+@import './assets/styles/base';
+
+* {
+  box-sizing: border-box;
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  -webkit-tap-highlight-color: transparent;
+}
+
+html {
+  font-family: $font-family;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-size: $font-size;
+  color: $font-color;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+
+body {
+  line-height: $line-height;
+}
+
+#app {
+
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-weight: normal;
+  margin-bottom: 1rem;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0;
+}
+
+a,
+a:visited {
+  color: black;
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+p {
+  margin-bottom: 0.5rem;
+}
+
+i, em {
+  font-style: italic;
+}
+
+strong {
+  font-weight: bold;
+}
+
+blockquote {
+  padding: 0 1rem;
+  position: relative;
+  display: inline-block;
+  margin-bottom: 1rem;
+
+  &:before {
+    content: "“";
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+
+  &:after {
+    content: "”";
+    position: absolute;
+    right: 0;
+    bottom: -0.5rem;
+  }
+}
+
+hr {
+  border-color: black;
+  margin: 1rem 0;
+  border-style: solid;
+}
+
+figure {
+  margin-bottom: 1rem;
+}
+
+figure.video {
+  height: 0;
+  padding-bottom: 56.25%;
+  position: relative;
+  background-color: lightgray;
+
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+}
+
+ul, ol {
+  li {
+    display: list-item;
+    padding-left: 0.75rem;
+    position: relative;
+
+    &:before {
+      content: '•';
+      position: absolute;
+      left: 0;
+    }
+
+    ul, ol {
+      margin-bottom: 1rem;
     }
   }
 }
